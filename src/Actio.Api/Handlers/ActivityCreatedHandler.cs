18e299a5c91280856017d70adf12a2d87eeeefbc
sajoratosg <geovani.succi@sage.com>
2019-03-15 13:51:51 -0300

    services implemented

---
+++ src/Actio.Api/Handlers/ActivityCreatedHandler.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Actio.Common.Events;
+
+namespace Actio.Api.Handlers
+{
+    public class ActivityCreatedHandler : IEventHandler<ActivityCreated>
+    {
+        public async Task HandleAsync(ActivityCreated @event)
+        {
+           await Task.CompletedTask;
+        }
+    }
+}
